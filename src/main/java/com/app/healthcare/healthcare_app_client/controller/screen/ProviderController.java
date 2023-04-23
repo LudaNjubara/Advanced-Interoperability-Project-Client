65bb3cbc10831339c365397fa5e26fa25d2224f6
LudaNjubara <mihael.sestak00@gmail.com>
2023-04-23 20:15:41 +0200

    Added ability to view provider's patients and appointments

--- src/main/java/com/app/healthcare/healthcare_app_client/controller/screen/ProviderController.java
+++ src/main/java/com/app/healthcare/healthcare_app_client/controller/screen/ProviderController.java
@@ -1,8 +1,11 @@
 package com.app.healthcare.healthcare_app_client.controller.screen;
 
+import com.app.healthcare.healthcare_app_client.model.Appointment;
 import com.app.healthcare.healthcare_app_client.model.Facility;
+import com.app.healthcare.healthcare_app_client.model.Patient;
 import com.app.healthcare.healthcare_app_client.model.Provider;
 import com.app.healthcare.healthcare_app_client.utils.Utils;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -15,8 +18,7 @@ import org.springframework.web.client.RestTemplate;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import static com.app.healthcare.healthcare_app_client.utils.Fetchers.fetchAllFacilities;
-import static com.app.healthcare.healthcare_app_client.utils.Fetchers.fetchAllProviders;
+import static com.app.healthcare.healthcare_app_client.utils.Fetchers.*;
 import static com.app.healthcare.healthcare_app_client.utils.Utils.setupRestTemplate;
 import static com.app.healthcare.healthcare_app_client.utils.Utils.setupTableViewContextMenu;
 
@@ -24,13 +26,21 @@ public class ProviderController implements Initializable {
 
     private Facility[] facilities;
 
+    // TABLES
     @FXML
     private TableView<Provider> currentProvidersTable;
+    @FXML
+    private TableView<Patient> currentPatientsTable;
+    @FXML
+    private TableView<Appointment> currentAppointmentsTable;
 
+    // PROVIDER TABLE COLUMNS
     @FXML
     private TableColumn<Provider, String> firstNameColumn;
     @FXML
     private TableColumn<Provider, String> lastNameColumn;
+    @FXML
+    private TableColumn<Provider, String> facilityColumn;
     @FXML
     private TableColumn<Provider, String> addressColumn;
     @FXML
@@ -42,7 +52,39 @@ public class ProviderController implements Initializable {
     @FXML
     private TableColumn<Provider, String> imageURLColumn;
 
+    // PATIENT TABLE COLUMNS
+    @FXML
+    private TableColumn<Patient, String> patientFirstNameColumn;
+    @FXML
+    private TableColumn<Patient, String> patientLastNameColumn;
+    @FXML
+    private TableColumn<Patient, String> patientFacilityColumn;
+    @FXML
+    private TableColumn<Patient, String> patientAddressColumn;
+    @FXML
+    private TableColumn<Patient, String> patientPhoneNumberColumn;
+    @FXML
+    private TableColumn<Patient, String> patientDOBColumn;
+    @FXML
+    private TableColumn<Patient, String> patientOibColumn;
+    @FXML
+    private TableColumn<Patient, String> patientImageURLColumn;
 
+    // APPOINTMENT TABLE COLUMNS
+    @FXML
+    private TableColumn<Appointment, String> appointmentTitleColumn;
+    @FXML
+    private TableColumn<Appointment, String> appointmentDescriptionColumn;
+    @FXML
+    private TableColumn<Appointment, String> appointmentFacilityColumn;
+    @FXML
+    private TableColumn<Appointment, String> appointmentProviderColumn;
+    @FXML
+    private TableColumn<Appointment, String> appointmentPatientColumn;
+    @FXML
+    private TableColumn<Appointment, String> appointmentDateColumn;
+
+    // TEXT FIELDS
     @FXML
     private TextField firstNameTextField;
     @FXML
@@ -60,6 +102,7 @@ public class ProviderController implements Initializable {
     @FXML
     private ComboBox<String> facilityComboBox;
 
+    // BUTTONS
     @FXML
     private Button addProviderButton;
     @FXML
@@ -68,19 +111,132 @@ public class ProviderController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        setupTable();
+        setupMainTable();
+        setupPatientsTable();
+        setupAppointmentsTable();
         setupFacilitiesComboBox();
-        ContextMenu contextMenu = setupTableViewContextMenu();
 
+        ContextMenu contextMenu = setupTableViewContextMenu();
         contextMenu.getItems().get(0).setOnAction(actionEvent -> handleContextItemClicked(contextMenu.getItems().get(0)));
         contextMenu.getItems().get(1).setOnAction(actionEvent -> handleContextItemClicked(contextMenu.getItems().get(1)));
-
         currentProvidersTable.setContextMenu(contextMenu);
+
+        // create method that will handle clicking on a row in the table
+        currentProvidersTable.getSelectionModel().selectedItemProperty().addListener(
+                (obs, oldSelection, newSelection) -> handleProviderSelection(newSelection)
+        );
 
         addProviderButton.setVisible(true);
         editProviderButton.setVisible(false);
     }
 
+    private void handleProviderSelection(Provider newSelection) {
+        if (newSelection != null) {
+            Patient[] patients = fetchAllPatientsByProviderId(newSelection.getId());
+            Appointment[] appointments = fetchAllAppointmentsByProviderId(newSelection.getId());
+
+            currentPatientsTable.getItems().clear();
+            currentPatientsTable.getItems().addAll(patients);
+
+            currentAppointmentsTable.getItems().clear();
+            currentAppointmentsTable.getItems().addAll(appointments);
+        }
+    }
+
+
+    // SETUP METHODS
+    private void setupMainTable() {
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        facilityColumn.setCellValueFactory(cellData -> {
+            Provider provider = cellData.getValue();
+            if (provider != null) {
+                Facility facility = provider.getFacility();
+                if (facility != null) {
+                    return new SimpleStringProperty(facility.getName());
+                }
+            }
+            return new SimpleStringProperty("");
+        });
+        addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+        phoneNumberColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
+        oibColumn.setCellValueFactory(new PropertyValueFactory<>("oib"));
+        imageURLColumn.setCellValueFactory(new PropertyValueFactory<>("imageURL"));
+
+        currentProvidersTable.getItems().clear();
+        currentProvidersTable.getItems().addAll(fetchAllProviders());
+    }
+
+    private void setupPatientsTable() {
+        patientFirstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        patientLastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        patientFacilityColumn.setCellValueFactory(cellData -> {
+            Patient patient = cellData.getValue();
+            if (patient != null) {
+                Facility facility = patient.getFacility();
+                if (facility != null) {
+                    return new SimpleStringProperty(facility.getName());
+                }
+            }
+            return new SimpleStringProperty("");
+        });
+        patientAddressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
+        patientPhoneNumberColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
+        patientDOBColumn.setCellValueFactory(new PropertyValueFactory<>("dateOfBirth"));
+        patientOibColumn.setCellValueFactory(new PropertyValueFactory<>("oib"));
+        patientImageURLColumn.setCellValueFactory(new PropertyValueFactory<>("imageURL"));
+
+        currentPatientsTable.getItems().clear();
+    }
+
+    private void setupAppointmentsTable() {
+        appointmentTitleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        appointmentDescriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        appointmentFacilityColumn.setCellValueFactory(cellData -> {
+            Appointment appointment = cellData.getValue();
+            if (appointment != null) {
+                Facility facility = appointment.getFacility();
+                if (facility != null) {
+                    return new SimpleStringProperty(facility.getName());
+                }
+            }
+            return new SimpleStringProperty("");
+        });
+        appointmentProviderColumn.setCellValueFactory(cellData -> {
+            Appointment appointment = cellData.getValue();
+            if (appointment != null) {
+                Provider provider = appointment.getProvider();
+                if (provider != null) {
+                    return new SimpleStringProperty(provider.getFirstName() + " " + provider.getLastName());
+                }
+            }
+            return new SimpleStringProperty("");
+        });
+        appointmentPatientColumn.setCellValueFactory(cellData -> {
+            Appointment appointment = cellData.getValue();
+            if (appointment != null) {
+                Patient patient = appointment.getPatient();
+                if (patient != null) {
+                    return new SimpleStringProperty(patient.getFirstName() + " " + patient.getLastName());
+                }
+            }
+            return new SimpleStringProperty("");
+        });
+        appointmentDateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        currentAppointmentsTable.getItems().clear();
+    }
+
+    private void setupFacilitiesComboBox() {
+        facilities = fetchAllFacilities();
+        for (Facility facility : facilities) {
+            facilityComboBox.getItems().add(facility.getName());
+        }
+    }
+
+
+    // CONTEXT MENU EVENT HANDLERS
     private void handleContextItemClicked(MenuItem menuItem) {
         if (menuItem.getText().equals("Delete")) {
             Provider provider = currentProvidersTable.getSelectionModel().getSelectedItem();
@@ -98,63 +254,28 @@ public class ProviderController implements Initializable {
 
         try {
             restTemplate.delete(PROVIDERS_URL + provider.getId());
-            setupTable();
+            setupMainTable();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void handleEditProviderContextItemClick(Provider provider) {
-        String FACILITIES_URL = "http://localhost:8081/api/facilities/";
-        RestTemplate rest = setupRestTemplate();
-
         firstNameTextField.setText(provider.getFirstName());
         lastNameTextField.setText(provider.getLastName());
+        facilityComboBox.setValue(provider.getFacility().getName());
         addressTextField.setText(provider.getAddress());
         phoneNumberTextField.setText(provider.getPhoneNumber());
         emailTextField.setText(provider.getEmail());
         oibTextField.setText(provider.getOib());
         imageURLTextField.setText(provider.getImageUrl());
-        Facility facility;
-
-        try {
-            ResponseEntity<Facility> responseEntity = rest.getForEntity(FACILITIES_URL + provider.getFacility().getId(), Facility.class);
-
-            if (responseEntity.getStatusCode().is2xxSuccessful()) {
-                facility = responseEntity.getBody();
-                assert facility != null;
-                facilityComboBox.setValue(facility.getName());
-            } else {
-                System.out.println("Request failed with status code: " + responseEntity.getStatusCode());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
         addProviderButton.setVisible(false);
         editProviderButton.setVisible(true);
     }
 
-    private void setupTable() {
-        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-        addressColumn.setCellValueFactory(new PropertyValueFactory<>("address"));
-        phoneNumberColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
-        emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
-        oibColumn.setCellValueFactory(new PropertyValueFactory<>("oib"));
-        imageURLColumn.setCellValueFactory(new PropertyValueFactory<>("imageURL"));
 
-        currentProvidersTable.getItems().clear();
-        currentProvidersTable.getItems().addAll(fetchAllProviders());
-    }
-
-    private void setupFacilitiesComboBox() {
-        facilities = fetchAllFacilities();
-        for (Facility facility : facilities) {
-            facilityComboBox.getItems().add(facility.getName());
-        }
-    }
-
+    // BUTTON EVENT HANDLERS
     @FXML
     public void handleEditProviderButtonClick() {
         Long id = currentProvidersTable.getSelectionModel().getSelectedItem().getId();
@@ -192,7 +313,7 @@ public class ProviderController implements Initializable {
             if (responseEntity.getStatusCode().is2xxSuccessful()) {
                 Utils.showNotification("Success", "Provider updated successfully.", editProviderButton);
                 clearFields();
-                setupTable();
+                setupMainTable();
             } else {
                 System.out.println("Request failed with status code: " + responseEntity.getStatusCode());
                 Utils.showNotification("Error", "Request failed with status code: " + responseEntity.getStatusCode(), addProviderButton);
@@ -240,6 +361,8 @@ public class ProviderController implements Initializable {
         }
     }
 
+
+    // CLEANUP
     private void clearFields() {
         firstNameTextField.clear();
         lastNameTextField.clear();
