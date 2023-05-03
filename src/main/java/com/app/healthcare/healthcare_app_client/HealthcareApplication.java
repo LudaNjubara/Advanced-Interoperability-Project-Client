93bb8130911cbdb3281bb5fd263ec551ab8c32a3
LudaNjubara <mihael.sestak00@gmail.com>
2023-05-03 20:36:12 +0200

    Changed default window size on app start

--- src/main/java/com/app/healthcare/healthcare_app_client/HealthcareApplication.java
+++ src/main/java/com/app/healthcare/healthcare_app_client/HealthcareApplication.java
@@ -11,7 +11,7 @@ public class HealthcareApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HealthcareApplication.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 900);
         stage.setTitle("Healthcare Application");
         stage.setScene(scene);
         stage.show();
